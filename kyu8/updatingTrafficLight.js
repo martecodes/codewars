d24fb3623311218928b51240d60a1d8bbf6ce97e
martecodes <jeanmarte93@gmail.com>
2021-10-13 21:41:35 -0400

    codewars

---
+++ kyu8/updatingTrafficLight.js
@@ -0,0 +1,5 @@
+function updateLight(current) {
+  
+    return current === 'green' ? current = 'yellow' : current === 'yellow' ? current = 'red' : current === 'red' ? current = 'green' : null 
+  
+  }
