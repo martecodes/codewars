5224f79cf4bdef0420949b489e3345155583012d
martecodes <jeanmarte93@gmail.com>
2021-10-28 09:16:08 -0400

    adding examples

--- kyu6/persistnetBugger.js
+++ kyu6/persistnetBugger.js
@@ -8,3 +8,9 @@ function persistence(num) {
 
   return count;
 }
+
+persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+// and 4 has only one digit
+
+persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+// 1*2*6 = 12, and finally 1*2 = 2
