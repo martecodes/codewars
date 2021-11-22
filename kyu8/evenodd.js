2319c6544bdda2711218dae72f174bfd8d38cccb
martecodes <jeanmarte93@gmail.com>
2021-11-22 14:10:40 -0500

    update

--- kyu8/evenodd.js
+++ kyu8/evenodd.js
@@ -1,3 +1,5 @@
 function even_or_odd(number) {
-  number % 2 === 0 ? "Even" : "Odd";
+  return number % 2 === 0 ? "Even" : "Odd";
 }
+
+console.log(even_or_odd(7))
--- kyu8/removeFirstCha.js
+++
@@ -1,7 +0,0 @@
-function removeChar(str){
-  newStr = str.split('')
-  delete newStr[0]
-  delete newStr[newStr.length -1]
-  newnew = newStr.join('')
-  return newnew
-};
