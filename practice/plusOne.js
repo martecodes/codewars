a6858b1c7891298cd5be44cc214f254995078fc5
martecodes <jeanmarte93@gmail.com>
2021-12-15 13:20:47 -0500

    plus one

---
+++ practice/plusOne.js
@@ -0,0 +1,15 @@
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4]
+
+const plusOne = function(digits) {
+    return ((BigInt(digits.join('')) + 1n).toString()).split('');
+};
