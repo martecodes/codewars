690b004a3b443f558d44dedd1021312a3f959528
martecodes <jeanmarte93@gmail.com>
2022-01-19 10:37:09 -0500

    codewars

--- kyu6/longestSubstringWithoutRepeating.js
+++ kyu6/longestSubstringWithoutRepeating.js
@@ -4,16 +4,31 @@ const lengthOfLongestSubstring = (s) =>{
     let map = {}
 
     for(let end = 0;end < s.length;end++){ // O(n)
+        //to check if i seen a character before.
+        //and the repeated character needs to be located at the index greater or equal to the begin index
         if(map[s[end]] !== undefined && map[s[end]] >= begin){
+            //if there is a duplicate character I'm going to update my starting point
             begin = map[s[end]] + 1
         }
+        //this will record at what index we seen each character
         map[s[end]] = end
+        
+        //example: begin = 0, end = 1 => subtring = "ab"
+            // 1 - 0 = 1 => "a" then we add 1 => "ab"
         max = Math.max(max, end - begin + 1)
     }
+
 
     return max
 }
 
-//if a string has 
+/*
+s = "ab00ghed"
 
-console.log(lengthOfLongestSubstring("ab00ghed"))
+begin = 0
+end = 0
+
+substring = "a"
+*/
+
+console.log(lengthOfLongestSubstring("abcbbcad"))
