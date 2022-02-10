0ea9cff299271913756038d3b007efe4c26451d0
martecodes <jeanmarte93@gmail.com>
2022-02-10 18:29:16 -0500

    codewars

--- kyu7/findMissingNumber.js
+++ kyu7/findMissingNumber.js
@@ -1,13 +1,14 @@
 function solution(A) {
     const mySet = new Set();
 
-    for (const num of A) {
+    for (const num of A) { // O(n)
         if (num > 0) {
             mySet.add(num);
         }
     }   
-    for (let i = 1; i <= A.length + 1; i++) {
-        if (!mySet.has(i)) {
+
+    for (let i = 1; i <= A.length + 1; i++) { // O(n)
+        if (!mySet.has(i)) { 
             return i;
         }
     }
